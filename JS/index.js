2d743badf1dc5fbd04cfe5f6790412aa5204edf4
Bennythagode <baruchbenb@gmail.com>
2021-05-11 00:54:42 +0300

    milestone4

--- JS/index.js
+++ JS/index.js
@@ -10,17 +10,24 @@ function generateFibo(x) {
     return sequence[x]
 }
 
+function fiboRequest(x) {
+    let url = "http://localhost:5050/fibonacci/";
+
+    $.get(
+        url + x, {},
+        function(data) {
+            let y = document.getElementById("Y");
+            y.innerText = data.result;
+
+        }
+    );
+
+}
+
+
 function calculate() {
     let x = document.getElementById("X").value;
     x = Number(x);
-    let y = document.getElementById("Y");
-    y.innerText = generateFibo(x);
-}
 
-function genetorFibRecursive(x) {
-    if (x < 2) {
-        return 1;
-    } else {
-        return genetorFibRecursive(x - 2) + genetorFibRecursive(x - 1);
-    }
+    fiboRequest(x);
 }
