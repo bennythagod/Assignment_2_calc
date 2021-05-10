7d3dbe545c369e4a051420b207d0beda6510316b
Bennythagode <baruchbenb@gmail.com>
2021-05-11 00:05:15 +0300

    milestone3

--- JS/index.js
+++ JS/index.js
@@ -3,14 +3,16 @@ function generateFibo(x) {
 
     sequence[0] = 0;
     sequence[1] = 1;
-    for (let i = 2; i < x; i++) {
+    for (let i = 2; i <= x; i++) {
         sequence[i] = sequence[i - 2] + sequence[i - 1];
 
     }
-    return sequence[x - 1]
+    return sequence[x]
 }
 
-let x = 5;
-let y = generateFibo(x);
-let elem = document.getElementById("declaring");
-elem.innerText = "The Fibonacci of " + x + " is " + y + ".";
+function calculate() {
+    let x = document.getElementById("X").value;
+    x = Number(x);
+    let y = document.getElementById("Y");
+    y.innerText = generateFibo(x);
+}
